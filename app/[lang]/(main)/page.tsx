d668ac60f73d11a7559a3254db2e15a57df57fef
Yunus <ibadovunus55@gmail.com>
2024-11-25 22:09:25 +0500

    fixed

---
+++ app/[lang]/(main)/page.tsx
@@ -0,0 +1,34 @@
+import Slider from "@/components/Corusel";
+import Image from "next/image";
+import Layout from "./Layout";
+import { getDictionary } from "../dictionaries";
+
+export default async function Home({ params: { lang }, }: { params: { lang: string }; }) {
+  const translation = await getDictionary(lang)
+  return (
+    <>
+      <Layout translation={translation}>
+        <div className="px-20">
+          <div className="flex gap-20">
+            <div className="text-[18px] flex flex-col gap-3.5 font-normal mt-9">
+              <div className="flex gap-14"><span>Woman’s Fashion</span><Image src="/images/yonba.svg" alt="hello" height={14} width={8} /> </div>
+              <div className="flex gap-20"><span>Men’s Fashion</span> <Image src="/images/yonba.svg" alt="hello" height={14} width={8} /></div>
+              <span>Electronics</span>
+              <span>Home & Lifestyle</span>
+              <span>Medicine</span>
+              <span>Sports & Outdoor</span>
+              <span>Baby’s & Toys</span>
+              <span>Groceries & Pets</span>
+              <span>Health & Beauty</span>
+            </div>
+            <div className="h-[420px] w-[1.6px] bg-gray-200"></div>
+            <div className="mt-9">
+              <Slider></Slider>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
---
+++ components/LanguageSwitch.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useRouter, usePathname } from 'next/navigation';
+import { i18n, Locale } from "@/i18n.config";
+
+const LanguageSwitcher = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const { locales, defaultLocale } = i18n;
+
+    console.log(locales);
+
+
+    const changeLanguage = (newLocale: Locale) => {
+        // Получаем текущий путь без локали
+        const segments = pathname.split('/').filter(Boolean);
+
+        // Удаляем текущую локаль, если она есть
+        if (locales.includes(segments[0] as Locale)) {
+            segments.shift();
+        }
+
+        // Создаём новый путь с выбранной локалью
+        const newPath = `/${newLocale}/${segments.join('/')}`;
+
+        // Перенаправляем на новый путь
+        router.push(newPath || `/${newLocale}`);
+    };
+
+    // Определяем текущую локаль
+    const currentLocale = locales.includes(pathname.split('/')[1] as Locale)
+        ? (pathname.split('/')[1] as Locale)
+        : defaultLocale;
+
+    return (
+
+        <select
+            value={currentLocale}
+            onChange={(e) => changeLanguage(e.target.value as Locale)}
+            className="absolute right-[5%] w-[auto] bg-black bg-none border-none outline-none text-white "
+        >
+            <option
+                className="bg-black text-white border-none outline-none"
+                key={locales[0]}
+                value={locales[0]}
+            >
+                {locales[0] === "ru" ? "Русский" : ""} 
+            </option>
+
+            <option
+                className="bg-black text-white border-none outline-none"
+                key={locales[1]}
+                value={locales[1]}
+            >
+                {locales[1] === "en" ? "English" : ""} 
+            </option>
+        </select>
+
+
+    );
+};
+
+export default LanguageSwitcher;
